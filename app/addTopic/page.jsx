ac15c1c296a52baf65659dc7c23c38d47c00a8de
Laurent Dislaire <siladire@free.fr>
2024-02-13 19:00:37 +0100

    creation de la page addTopic

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,3 +1,17 @@
-export default function AddTopic(){
-  return <div>Ajouter un article</div>
+export default function AddTopic() {
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Titre de l'article"
+      />
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Description de l'article"
+      />
+      <button className="bg-blue-500 font-bold text-white py-3 px-6 w-fit">Ajouter article</button>
+    </form>
+  );
 }
