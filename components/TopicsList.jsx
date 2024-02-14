f0e64c1ba7daedd01c2d0460d2d12fa69fed888f
Laurent Dislaire <siladire@free.fr>
2024-02-14 17:37:43 +0100

    Fetch et affichage des data de topic dans la vue

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,22 +1,42 @@
+/* eslint-disable react/jsx-key */
 import RemoveBtn from "@/components/RemoveBtn";
 import Link from "next/link";
 import { HiPencilAlt } from "react-icons/hi";
 
-export default function TopicsList() {
+const getTopics = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/topics", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Fetch a échoué");
+    }
+    return res.json();
+  } catch (error) {
+    console.log("Erreur de chargement d'article: ", error);
+  }
+};
+
+export default async function TopicsList() {
+  const {topics } = await getTopics()
   return (
     <>
-      <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
+      { topics.map((t) => (
+
+        <div className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start">
         <div>
-          <h2 className="font-bold text-2xl">Titre Article</h2>
-          <div>{"Description de l'article"}</div>
+          <h2 className="font-bold text-2xl">{t.title}</h2>
+          <div>{t.description}</div>
         </div>
         <div className="flex gap-2">
-          <RemoveBtn/>
-          <Link href={'/editTopic/123'}>
+          <RemoveBtn />
+          <Link href={`/editTopic/${t._id}`}>
             <HiPencilAlt size={24} />
           </Link>
         </div>
       </div>
+     ))}
     </>
   );
 }
