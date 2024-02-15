c24b77aa1505f1f17ae6e3632ef692dd03705466
Laurent Dislaire <siladire@free.fr>
2024-02-15 14:39:29 +0100

    implementation de la suppression d'un topic

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -1,17 +1,65 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function AddTopic() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!title || !description) {
+      alert("Le title et la description sont requis.");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:3000/api/topics", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ title, description }),
+      });
+
+      if (res.ok) {
+        router.push("/");
+      } else {
+        throw new Error("La créatoin d'article a échoué");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
       <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
         className="border border-slate-500 px-8 py-2"
         type="text"
-        placeholder="Titre de l'article"
+        placeholder="Titre Article"
       />
+
       <input
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
         className="border border-slate-500 px-8 py-2"
         type="text"
-        placeholder="Description de l'article"
+        placeholder="Description de l'Article"
       />
-      <button className="bg-blue-400 font-bold text-white py-3 px-6 w-fit">Ajouter article</button>
+
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        Ajouter
+      </button>
     </form>
   );
 }
--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,7 +1,23 @@
-import { HiOutlineTrash } from "react-icons/hi"
+"use client";
+import { useRouter } from "next/navigation";
+import { HiOutlineTrash } from "react-icons/hi";
 
-export default function RemoveBtn(){
-  return <button className="text-red-400" >
-    <HiOutlineTrash size={24}/>
-  </button>
+export default function RemoveBtn({ id }) {
+  const router = useRouter();
+  const removeTopic = async () => {
+    const confirmed = confirm("Confirmer la suppression ?");
+    console.log(`Suppression du topic avec l'ID ${id}`);
+    if (confirmed) {
+      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok)  {router.refresh()};
+    }
+  };
+
+  return (
+    <button onClick={removeTopic} className="text-red-400">
+      <HiOutlineTrash size={24} />
+    </button>
+  );
 }
